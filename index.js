f37e7986e792377e21e73854bc1b36eb2793b4bd
Juwelmia63 <designghor5@gmail.com>
2024-08-16 00:13:32 +0600

    all food item add

--- index.js
+++ index.js
@@ -30,14 +30,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
+    const allfooddata = client.db('restrobiz').collection('allfood');
+
+    app.get('/allfood', async (req, res)=>{
+      const result = await allfooddata.find().toArray();
+
+      res.send(result);
+    })
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+   
   }
 }
 run().catch(console.dir);
