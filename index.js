0f494e55fcb4011bf75d26db6948923227e8cc6b
Juwelmia63 <designghor5@gmail.com>
2024-08-16 11:52:51 +0600

    fetching single food api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,10 +33,23 @@ async function run() {
     
     const allfooddata = client.db('restrobiz').collection('allfood');
 
+
+    // geting all food data
+
     app.get('/allfood', async (req, res)=>{
       const result = await allfooddata.find().toArray();
 
       res.send(result);
+    })
+
+
+    // geting single food data using id
+
+    app.get('/singlefoodpage/:id', async (req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await allfooddata.findOne(query)
+      res.send(result)
     })
    
     // Send a ping to confirm a successful connection
