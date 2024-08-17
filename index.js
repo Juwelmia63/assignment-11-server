3b2ff16562b8e13f5bafdba72b1bcb1d75a85adc
Juwelmia63 <designghor5@gmail.com>
2024-08-17 12:43:13 +0600

    added my food item

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ async function run() {
       const result = await allfooddata.insertOne(addfooddata)
       res.send(result)
     })
+    // get all food item by email
+
+    app.get('/addfood/:email', async (req, res)=>{
+      const email = req.params.email
+      const query= {'email':email}
+      const result = await allfooddata.find(query).toArray()
+      res.send(result)
+    })
    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
